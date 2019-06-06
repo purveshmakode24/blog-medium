b39f7950a45a5be47ec969d1b0aa6a0de057ceed
Purvesh Makode <makodepurvesh@gmail.com>
2019-06-06 23:27:08 +0530

    Add Password reset support

--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-import filters
+# import filters
 
 
 # Create your views here.
