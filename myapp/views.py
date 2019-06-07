202af6d06460e6ed0977d993a77b80c99fe3f785
Purvesh Makode <makodepurvesh@gmail.com>
2019-06-07 19:58:36 +0530

    Add email field in Signup page

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Post
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .forms import UserRegisterForm
 # import filters
 
 
@@ -22,14 +23,14 @@ def home(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your Account for {username} has been created Successfully')
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
 
     context = {
         'form': form
