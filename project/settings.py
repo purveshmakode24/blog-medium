379be58784a4084988898644acec9ddf6fb27d09
Purvesh Makode <makodepurvesh@gmail.com>
2019-08-23 23:51:21 +0530

    c

--- project/settings.py
+++ project/settings.py
@@ -172,6 +172,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
