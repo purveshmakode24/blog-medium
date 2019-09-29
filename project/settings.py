4e0cbeae488f630fa479be7c4890786c33691194
Purvesh Makode <makodepurvesh@gmail.com>
2019-09-30 03:25:26 +0530

    Update debug code for prod & dev

--- project/settings.py
+++ project/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = '9@9-l!r@%)^uyg0jppqvmd^cro0f=(xv6dy-w8l9e*b8=sk+=u'
 
 # DEBUG = os.environ.get('DJANGO_DEBUG') != 'False'
 # or
-# DEBUG = os.environ.get('DJANGO_DEBUG') is not False
+DEBUG = os.environ.get('DJANGO_DEBUG') != 'False'
 
-DEBUG = False
+# DEBUG = False
 
 # ALLOWED_HOSTS = ['blog-medium.herokuapp.com', '127.0.0.1']
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'blog-medium.herokuapp.com']
